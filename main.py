02c76aef0cc195f5e589453f091923288ec27b6e
isaacbarcroft <i_barcroft@yahoo.com>
2021-10-08 17:18:10 -0400

    Level One fixed

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ player_count = 0
 options = ['1', '2', '3','4', '5', '6', '7', '8', '9', '10']
 index = randint(1, 10)
 
-while player_count < 3:
+while player_count <= 3:
     selection = input('1,2,3,4,5,6,7,8,9,10')
     
     if selection == options[index]:
-
         print("You guessed " + options[index] + " You win!")
+        break
+    elif(int(selection) > index):
+        player_count += 1
+        print('You guessed too high, try again')
+        
+    elif(int(selection) < index):
+        player_count += 1
+        print('You guessed too low')
+       
     else: 
-        print("you lose")
-        player_count = player_count + 1
-        if player_count == 3:
-            print("You didnt guess " + options[index] + " Game Over")
+        print("You didnt guess " + options[index] + " Game Over!", player_count, "guesses too many")
+        break
